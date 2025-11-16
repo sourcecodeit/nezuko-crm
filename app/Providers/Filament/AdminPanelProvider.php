3c2c450cd4a3963207ed63deea78c687d67fa60a
Diego Imbriani <diego@sourcecode.team>
2025-11-16 18:16:46 +0100

    feat: enable database notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('')
             ->login()
             ->brandLogo('/images/logo.png')
+            ->databaseNotifications()
             ->colors([
                 'primary' => Color::Amber,
             ])
